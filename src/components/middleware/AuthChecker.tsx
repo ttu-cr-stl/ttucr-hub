f698a1f3cc521457715ee4f1d4500abd567e6396
axantillon <aantillon.leiva@gmail.com>
2024-05-30 20:42:28 -0600

    add splash screen and change where login redirects

--- src/components/middleware/AuthChecker.tsx
+++ src/components/middleware/AuthChecker.tsx
@@ -1,25 +1,30 @@
-'use client'
-import { usePrivy } from '@privy-io/react-auth';
-import { useRouter } from 'next/navigation';
-import { FC, ReactNode } from 'react';
+"use client";
+import { usePrivy } from "@privy-io/react-auth";
+import { useRouter } from "next/navigation";
+import { FC, ReactNode } from "react";
+import { useLocalStorage } from "usehooks-ts";
+import { SplashScreen } from "../utils/SplashScreen";
 
-const AuthChecker: FC<{children: ReactNode}> = ({ children }) => {
+const AuthChecker: FC<{ children: ReactNode }> = ({ children }) => {
+  const { ready, authenticated } = usePrivy();
+  const router = useRouter();
+  const [prevAuth] = useLocalStorage("prev-authenticated", false);
 
-    const {ready, authenticated} = usePrivy();
-    const router = useRouter();
+  if (!prevAuth) {
+    router.push("/login");
+    return <SplashScreen />;
+  }
 
-    if (!ready) {
-        // Do nothing while the PrivyProvider initializes with updated user state
-        return <><span>loading...</span></>;
-    }
+  if (!ready) {
+    // Do nothing while the PrivyProvider initializes with updated user state
+    return <SplashScreen />;
+  }
 
-    if (ready && !authenticated) {
-        router.push('/')
-    }
+  if (ready && !authenticated) {
+    router.push("/login");
+  }
 
-    return (
-        <>{authenticated && <>{children}</>}</>
-    )
-}
+  return <>{authenticated && <>{children}</>}</>;
+};
 
 export default AuthChecker;
---
+++ src/components/utils/SplashScreen.tsx
@@ -0,0 +1,5 @@
+export const SplashScreen = () => (
+  <main className="relative h-dvh w-dvw flex items-center justify-center">
+    <span>Splash Screen...</span>
+  </main>
+);
