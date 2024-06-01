f8d8034066775b87547ff29d4a48b7563a05cf1e
axantillon <aantillon.leiva@gmail.com>
2024-06-01 14:51:08 -0600

    add welcome messsage in login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,11 @@ import LoginBtn from "@/components/utils/LoginBtn";
 
 export default function Login() {
   return (
-    <main className="relative h-full w-full flex flex-col items-center justify-center">
+    <main className="relative h-full w-full flex flex-col items-center justify-center space-y-10">
+      <span className="text-lg font-bold text-center">
+        Welcome to <b>TTU@CR HUB,</b> <br />
+        <span className="text-accent">Red Raider</span>!
+      </span>
       <LoginBtn />
     </main>
   );
