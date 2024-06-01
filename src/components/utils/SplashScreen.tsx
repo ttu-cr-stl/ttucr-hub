443cfedf47c85e54c86014b4a0808104f471bef8
axantillon <aantillon.leiva@gmail.com>
2024-06-01 14:25:09 -0600

    update legacy props on Image comp

--- src/components/utils/SplashScreen.tsx
+++ src/components/utils/SplashScreen.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export const SplashScreen = () => (
   <main className="relative h-dvh w-dvw flex items-center justify-center">
     <div className="relative w-2/3 h-96 aspect-auto">
-      <Image src="/LogoSTL.png" alt="" layout="fill" objectFit="contain" />
+      <Image
+        src="/LogoSTL.png"
+        alt=""
+        fill
+        style={{
+          objectFit: "contain",
+        }}
+      />
     </div>
   </main>
 );
