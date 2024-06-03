0fef3356327092ff727c6a1c1ec5582e33155c5e
axantillon <aantillon.leiva@gmail.com>
2024-06-03 13:19:01 -0600

    create hook for logged in user info

---
+++ src/lib/hooks/useUser.tsx
@@ -0,0 +1,19 @@
+import { getUserByEmail } from "@/db/users";
+import { User } from "@prisma/client";
+import { usePrivy } from "@privy-io/react-auth";
+import { useEffect, useState } from "react";
+
+export const useUser = () => {
+  const [user, setUser] = useState<User | null>(null);
+  const { user: privyUser } = usePrivy();
+
+  useEffect(() => {
+    if (privyUser?.email?.address) {
+      getUserByEmail(privyUser.email?.address).then((user) => {
+        setUser(user);
+      })
+    }
+  });
+
+  return { user };
+};
