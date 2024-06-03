930ec7db4ff55b9c3576d7d5a54c8a1fd9b0328f
axantillon <aantillon.leiva@gmail.com>
2024-06-03 13:20:26 -0600

    create createUser functions

--- src/db/users/index.ts
+++ src/db/users/index.ts
@@ -1,16 +1,30 @@
 "use server";
 import prisma from "@/db/prisma";
 
+export async function createUser(email: string) {
+  const user = await prisma.user.create({
+    data: {
+      firstName: "",
+      lastName: "",
+      r_number: "",
+      major: "",
+      email: email,
+    }
+  });
+
+  return user;
+}
+
 export async function getAllUsers() {
   const users = await prisma.user.findMany();
 
   return users;
 }
 
-export async function getUserById(id: string) {
+export async function getUserByEmail(email: string) {
   const user = await prisma.user.findUnique({
     where: {
-      id
+      email
     },
   });
 
