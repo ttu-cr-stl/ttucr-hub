fa764b456eb4079d49c35506bd107f81f524c968
axantillon <aantillon.leiva@gmail.com>
2024-06-04 14:39:38 -0600

    temp

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,58 +1,62 @@
-'use client'
+"use client";
 import { cn } from "@/lib/utils/cn";
 import { NavPath } from "@/lib/utils/consts";
-import Link from 'next/link';
+import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { FC, ReactNode } from 'react';
-import { BarChart, CreditCard, Home, Search, Settings } from 'react-feather';
+import { FC, ReactNode } from "react";
+import { BarChart, CreditCard, Home, Search, Settings } from "react-feather";
 
+interface NavBarProps {}
+const NavBar: FC<NavBarProps> = ({}) => {
+  const activePath = usePathname();
 
-interface NavBarProps {
-  
-}
-const NavBar: FC<NavBarProps> = ({  }) => {
+  return (
+    <div
+      className={
+        "absolute bottom-0 left-0 w-full h-16 border-t flex items-center justify-evenly z-10"
+      }
+    >
+      <NavIcon pathname={NavPath.HOME} activePath={activePath}>
+        <Home />
+      </NavIcon>
 
-    const activePath = usePathname();
+      <NavIcon pathname={NavPath.LEADERBOARD} activePath={activePath}>
+        <BarChart className=" -scale-x-100" />
+      </NavIcon>
 
-    return (
-        <div className={'absolute bottom-0 left-0 w-full h-16 border-t flex items-center justify-evenly z-10'}>
-            <NavIcon pathname={NavPath.HOME} activePath={activePath}>
-                <Home />
-            </NavIcon>
+      <NavIcon pathname={NavPath.EXPLORE} activePath={activePath}>
+        <Search />
+      </NavIcon>
 
-            <NavIcon pathname={NavPath.LEADERBOARD} activePath={activePath}>
-                <BarChart className=' -scale-x-100' />
-            </NavIcon>
+      <NavIcon pathname={NavPath.WALLET} activePath={activePath}>
+        <CreditCard />
+      </NavIcon>
 
-            <NavIcon pathname={NavPath.EXPLORE} activePath={activePath}>
-                <Search />
-            </NavIcon>
+      <NavIcon pathname={NavPath.SETTINGS} activePath={activePath}>
+        <Settings />
+      </NavIcon>
+    </div>
+  );
+};
 
-            <NavIcon pathname={NavPath.WALLET} activePath={activePath}>
-                <CreditCard />
-            </NavIcon>
-
-            <NavIcon pathname={NavPath.SETTINGS} activePath={activePath}>
-                <Settings />
-            </NavIcon>
-        </div>
-    )
-}
-
-const NavIcon: FC<{ children: ReactNode, pathname: NavPath, activePath: string }> = ({ children, pathname, activePath }) => {
-
-    return (
-      <Link href={pathname}>
-        <div
-          className={cn(
-          activePath.match(/\/[^\/]*/)![0] == pathname && "bg-primary/20",
-            "p-2 rounded-xl"
-          )}
-        >
-          {children}
-        </div>
-      </Link>
-    );
-}
+const NavIcon: FC<{
+  children: ReactNode;
+  pathname: NavPath;
+  activePath: string;
+}> = ({ children, pathname, activePath }) => {
+  return (
+    <Link href={pathname}>
+      <div
+        className={cn(
+          //TODO Find regex that matches for root
+          activePath == pathname && "bg-stone-400/20",
+          "p-2 rounded-xl"
+        )}
+      >
+        {children}
+      </div>
+    </Link>
+  );
+};
 
 export default NavBar;
