5eed99ad79cc5fa44df5faa7e0a19b6989b34a3d
axantillon <aantillon.leiva@gmail.com>
2024-06-03 18:42:18 -0600

    create updateProfile comp

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -1,16 +1,9 @@
-import { OrgsInfo } from "@/components/pages/onboarding/OrgsInfo"
-import { PersonalInfo } from "@/components/pages/onboarding/PersonalInfo"
-
-const OnboardingSteps = [
-    <PersonalInfo key='personal' />,
-    <OrgsInfo key='orgs' />
-]
+import { UpdateProfile } from "@/components/utils/UpdateProfile";
 
 export default function Onboarding() {
-    
-    return (
-        <main className="relative h-full w-full flex flex-col items-center justify-center space-y-10">
-            {OnboardingSteps[0]}
-        </main>
-    )
+  return (
+    <main className="relative h-full w-full flex flex-col p-4">
+      <UpdateProfile />
+    </main>
+  );
 }
---
+++ src/components/utils/UpdateProfile.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useUser } from "@/lib/hooks/useUser";
+import { FC } from "react";
+
+
+export const UpdateProfile = () => {
+  const { user } = useUser();
+
+  return <div className="">jhfkje</div>;
+};
