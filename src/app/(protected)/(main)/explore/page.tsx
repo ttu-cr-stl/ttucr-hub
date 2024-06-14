d2a87c4b2dfbe0495ed293a7057eacdfefb2da07
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-06-14 13:03:25 -0600

    moved map logic to OrgsList.tsx to not use client rendering

--- src/app/(protected)/(main)/explore/page.tsx
+++ src/app/(protected)/(main)/explore/page.tsx
@@ -1,34 +1,8 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import OrgCard from "@/components/ui/OrgCard";
-import { Org } from "@prisma/client";
-import { getAllOrgs } from "@/db/orgs";
+import React from "react";
+import OrgsList from "./OrgsList";
 
 export default function Explore() {
-  const [orgs, setOrgs] = useState<Org[]>([]);
-  const [error, setError] = useState<boolean>(false);
-
-  useEffect(() => {
-    async function fetchOrgs() {
-      try {
-        const data = await getAllOrgs();
-        setOrgs(data);
-      } catch (error) {
-        console.log('Error fetching data from the database:', error);
-        setError(true);
-      }
-    }
-
-    fetchOrgs();
-  }, []);
-
   return (
-    <div>
-      {orgs.map((org) => (
-        <OrgCard key={org.id} org={org} />
-      ))}
-
-      {error && <span>An error has occurred</span>}
-    </div>
+    <OrgsList />
   );
 }
