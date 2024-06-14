e8b0891f279e13b552818014562fb73a9d607d5c
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-06-14 13:18:17 -0600

    formatting

--- src/app/(protected)/(main)/explore/OrgsList.tsx
+++ src/app/(protected)/(main)/explore/OrgsList.tsx
@@ -2,14 +2,15 @@ import { getAllOrgs } from "@/db/orgs";
 import OrgCard from "@/components/ui/OrgCard";
 
 async function OrgsList() {
-    const orgs = await getAllOrgs();
-    return (
-        <div>
-            {orgs.map((org) => (
-                <OrgCard key={org.id} org={org} />
-            ))}
-        </div>
-    );
+  const orgs = await getAllOrgs();
+  return (
+    <div>
+      {orgs.map((org) => (
+        <OrgCard key={org.id} org={org} />
+      ))}
+    </div>
+  );
 }
 
 export default OrgsList;
+
