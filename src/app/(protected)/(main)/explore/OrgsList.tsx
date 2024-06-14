c9aa984a120fb4e43027d0afa365e196692b8eda
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-06-14 16:38:46 -0600

    added error handling to OrgsList

--- src/app/(protected)/(main)/explore/OrgsList.tsx
+++ src/app/(protected)/(main)/explore/OrgsList.tsx
@@ -2,15 +2,13 @@ import { getAllOrgs } from "@/db/orgs";
 import OrgCard from "@/components/ui/OrgCard";
 
 async function OrgsList() {
-  const orgs = await getAllOrgs();
-  return (
-    <div>
-      {orgs.map((org) => (
-        <OrgCard key={org.id} org={org} />
-      ))}
-    </div>
-  );
+  try {
+    const orgs = await getAllOrgs();
+    return orgs.map((org) => <OrgCard key={org.id} org={org} />);
+  } catch (error) {
+    console.log(error);
+    return <span>Something went wrong</span>;
+  }
 }
 
 export default OrgsList;
-
