9dfd837f49a0b532c3657f7f6997c63b1a17ffcc
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-06-14 16:51:07 -0600

    Back to default button

--- src/components/ui/OrgCard.tsx
+++ src/components/ui/OrgCard.tsx
@@ -1,29 +1,29 @@
 import React from "react";
-import { Org } from "@prisma/client";
-import * as card from "./card";
 import Link from "next/link";
 import Image from "next/image";
+import { Org } from "@prisma/client";
+import { Button } from "./button";
+import { Card, CardHeader, CardContent, CardFooter, CardTitle, CardDescription } from "./card";
 
 function OrgCard({ org }: { org: Org }) {
   return (
-    <Link href={`/org/${org.id}`}>
-      <card.Card className="mt-2">
-        <card.CardHeader>
-          <div className="flex items-center justify-between">
-            <card.CardTitle>{org.name}</card.CardTitle>
-            <Image src={"/TTULogo.png"} alt={org.name} width={75} height={75} />
-            {/* TODO: Use the actual org picture when they are available */}
-          </div>
-        </card.CardHeader>
-        <card.CardContent>
-          <card.CardDescription>{org.description}</card.CardDescription>
-        </card.CardContent>
-        <card.CardFooter
-          className="rounded-br-lg rounded-bl-lg border border-stone-200 P-0"
-          style={{ backgroundColor: `${org.color}` }}
-        />
-      </card.Card>
-    </Link>
+    <Card className="mt-2">
+      <CardHeader>
+        <div className="flex items-center justify-between">
+          <CardTitle>{org.name}</CardTitle>
+          <Image src={"/TTULogo.png"} alt={org.name} width={75} height={75} />
+          {/* TODO: Use the actual org picture when they are available */}
+        </div>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{org.description}</CardDescription>
+      </CardContent>
+      <CardFooter>
+        <Link href={`/org/${org.id}`}>
+          <Button>View Org</Button>
+        </Link>
+      </CardFooter>
+    </Card>
   );
 }
 
