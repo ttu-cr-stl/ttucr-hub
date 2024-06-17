1414befb1f5c169f6e8ff6ad3f7aaa55e020a566
Keisy <keisy.nf@gmail.com>
2024-06-16 21:20:59 -0600

    refactor UserCard component

--- src/components/ui/UserCard.tsx
+++ src/components/ui/UserCard.tsx
@@ -16,6 +16,7 @@ import {
 import Link from "next/link";
 import { Badge } from "@/components/ui/shadcn/badge";
 import { Degree } from "@/lib/utils/consts";
+import { Button } from "@/components/ui/shadcn/button";
 
 function UserCard({ user }: { user: User }) {
   const userMajor = Degree.find((degree) => degree.value === user.major);
@@ -28,20 +29,32 @@ function UserCard({ user }: { user: User }) {
     <Link href={`/user/${user.username}`}>
       <Card className="mt-2 border-2">
         <CardHeader>
-          <div className="flex items-center justify-between">
-            <CardTitle className="leading-relaxed">
-              {user.firstName} {user.lastName}
-            </CardTitle>
-            <Avatar className="w-24 h-24 ml-6">
+          <div className="flex items-center justify-left">
+            <Avatar className="w-24 h-24 mr-6">
               <AvatarImage src="" />
               <AvatarFallback>
                 {user.firstName[0]}
                 {user.lastName[0]}
               </AvatarFallback>
             </Avatar>
+            <div className="flex flex-col justify-start">
+              <CardTitle className="leading-relaxed">
+                {user.firstName}
+                <div className="font-normal">{user.lastName}</div>
+              </CardTitle>
+              <div className="flex flex-wrap">
+                <div className="mr-2 text-lg">{userMajor?.name}</div>
+                {userMinor?.name && user.minor !== "NONE" ? (
+                  <div className="">{userMinor?.name}</div>
+                ) : null}
+              </div>
+            </div>
+            {/* <Link href={`/user/${user.id}`}>
+              <Button>View User</Button>
+            </Link> */}
           </div>
         </CardHeader>
-        <CardContent className="flex items-centers">
+        {/* <CardContent className="flex items-centers">
           <Badge
             className="w-24 h-10 flex items-center justify-center text-center border-2 mt-[-10px] font-semibold"
             // variant="outline"
@@ -60,14 +73,13 @@ function UserCard({ user }: { user: User }) {
               {userMinor?.name}
             </Badge>
           ) : null}
-        </CardContent>
+        </CardContent> */}
         <CardFooter
           className="rounded-b-md"
           style={{
-            height: "4px",
             backgroundColor: userMajor ? userMajor.color : "pink",
           }}
-        />
+        ></CardFooter>
       </Card>
     </Link>
   );
