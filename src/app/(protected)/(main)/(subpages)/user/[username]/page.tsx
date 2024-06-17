eef59d8ae59479aa3ca80174bba064f937756ea1
Hans Buhrs Aguilar <hbuhrsag@ttu.edu>
2024-06-17 14:25:55 -0600

    initial-commit user's page

--- src/app/(protected)/(main)/(subpages)/user/[username]/page.tsx
+++ src/app/(protected)/(main)/(subpages)/user/[username]/page.tsx
@@ -1,5 +1,6 @@
 import { getUserByUsername } from "@/db/users";
-
+import { DegreeKeys, SelectOption } from "@/lib/types";
+import { getDegreeByKey } from "@/lib/utils";
 export default async function User({
   params,
 }: {
@@ -9,9 +10,16 @@ export default async function User({
 
   if (!user) return <div>User not found</div>;
 
+  const degree = getDegreeByKey(user.major as DegreeKeys)
+
   return (
-    <div className="">
-      <span>{user.firstName}</span>
+    <div className="w-auto h-auto bg-[#ececec] rounded-lg text-center p-3 inline-grid justify-center">
+      <h2>{user.firstName} {user.lastName}</h2>
+      <h4><span className="text-[#cc0000]">R-Number:</span> {user.r_number}</h4>
+      <div className="w-fit px-2 pt-0.5 pb-1 mt-2 rounded-full border" style={{color: degree.color, borderColor: degree.color}}>
+        <span>{degree.name}</span>
+      </div>
+      <span>{user.minor == "NONE" ? false : user.minor}</span>
     </div>
   );
 }
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,5 +1,9 @@
+import { SelectOption } from "../types";
+import { Degree } from "./consts";
 
 
 export const isTTUEmail = (email: string) => /@ttu\.edu/.test(email);
 
 export const extractUsername = (email: string) => email.split("@")[0];
+
+export const getDegreeByKey = (key: SelectOption['value']) => Degree[Degree.map((d) => d.value).indexOf(key)]
