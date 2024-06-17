a702cab6d7d91aca76b669d606576161a89b7d22
Keisy <keisy.nf@gmail.com>
2024-06-16 20:00:33 -0600

    Created UsersList.tsx with map logic to not use client rendering

--- src/app/(protected)/(main)/leaderboard/page.tsx
+++ src/app/(protected)/(main)/leaderboard/page.tsx
@@ -1,26 +1,6 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import UserCard from "@/components/ui/UserCard";
-import { User } from "@prisma/client";
-import { getAllUsers } from "@/db/users";
+import React from "react";
+import UsersList from "./UsersList";
 
-export default function LeaderboardPage() {
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    async function fetchUsers() {
-      const data = await getAllUsers();
-      setUsers(data);
-    }
-
-    fetchUsers();
-  }, []);
-
-  return (
-    <div className="">
-      {users.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
-    </div>
-  );
+export default function Leaderboard() {
+  return <UsersList />;
 }
