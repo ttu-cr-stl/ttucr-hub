135a56154b9544352b7f787a98aca9d65bbccd91
axantillon <aantillon.leiva@gmail.com>
2024-06-26 14:54:25 -0600

    update degree function

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -6,4 +6,5 @@ export const isTTUEmail = (email: string) => /@ttu\.edu/.test(email);
 
 export const extractUsername = (email: string) => email.split("@")[0];
 
-export const getDegreeByKey = (key: SelectOption['value']) => Degree[Degree.map((d) => d.value).indexOf(key)]
+export const getDegreeByKey = (key: string) =>
+  Degree.find((degree) => degree.value === key);
