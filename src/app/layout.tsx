b22b1ef116bfe3c9c861e4769395a1a9ecf87f37
axantillon <aantillon.leiva@gmail.com>
2024-07-10 11:56:18 -0600

    enable PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,46 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const APP_NAME = "TTU@CR Hub";
+const APP_DEFAULT_TITLE = "TTU@CR Hub";
+const APP_TITLE_TEMPLATE = "%s - TTU@CR Hub";
+const APP_DESCRIPTION =
+  "The place to catch up on Campus Activities, Student Organizations & fellow Students at the TTU@CR Campus. Built for Students, by Students.";
 
 export const metadata: Metadata = {
-  title: "TTU@CR Hub",
-  description:
-    "The place to catch up on Campus Activities, Student Orgs & fellow Students",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export const viewport: Viewport = {
@@ -18,7 +52,10 @@ export const viewport: Viewport = {
   userScalable: false,
   initialScale: 1,
   maximumScale: 1,
+  themeColor: "#FFFFFF",
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -26,8 +63,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className="relative self-center h-dvh w-dvw md:h-[667px] md:w-[375px] bg-stone-300 *:bg-white text-black">
+    <html lang="en" suppressHydrationWarning className={inter.className}>
+      <body className="relative self-center h-dvh w-dvw md:h-[667px] md:w-[375px] bg-[#F5F5F5] text-black">
         <Analytics />
         <SpeedInsights />
 
