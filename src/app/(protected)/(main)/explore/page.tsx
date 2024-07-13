6f493a2062ed35c21d7c4be259de9a00de9ba579
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-07-12 19:52:15 -0600

    added expolore page search field

--- src/app/(protected)/(main)/explore/page.tsx
+++ src/app/(protected)/(main)/explore/page.tsx
@@ -3,6 +3,15 @@ import React from "react";
 
 export default function Explore() {
   return (
-    <OrgsList />
+    <>
+      <input
+        className="m-5 border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-3xl text-sm focus:outline-none"
+        type="search"
+        id="orgsearch"
+        name="orgsearch"
+        placeholder="Search TTU@CR"
+      />
+      <OrgsList />
+    </>
   );
 }
