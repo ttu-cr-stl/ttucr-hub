039e9bae3144d2574d6c5031a169d7f7f1815228
Santiago Jimenez <santiago.jimenez.casas@gmail.com>
2024-07-14 16:33:32 -0600

    Finished redesign, still have to update org 2 to work correctly (its structure is outdated), and added logic to change grid's number of columns depending on the number of officers

--- src/app/(protected)/(main)/(subpages)/org/[id]/page.tsx
+++ src/app/(protected)/(main)/(subpages)/org/[id]/page.tsx
@@ -11,10 +11,6 @@ import { format, formatDistance } from "date-fns";
 
 export default async function Org({ params }: { params: { id: string } }) {
   const org = await getOrgById(params.id);
-  const user = await getUserByUsername("andysanc");
-
-  console.log(org);
-  console.log(user);
 
   if (!org) return <div>Organization not found</div>;
 
@@ -22,6 +18,17 @@ export default async function Org({ params }: { params: { id: string } }) {
     .split(" ")
     .map((word) => word[0])
     .join("");
+
+  const getUserPic = async (username: string) => {
+    let user;
+    try {
+      user = await getUserByUsername(username);
+    }
+    catch (e) {
+      console.error
+    }
+    return user?.profilePic;
+  }
 
   return (
     <div className="flex flex-col items-center">
@@ -31,27 +38,58 @@ export default async function Org({ params }: { params: { id: string } }) {
       </Avatar>
 
       <span className="mt-1 text-3xl text-gray-800">{org.name}</span>
-      <span className="text-sm text-gray-500">Established: {format(org.createdAt, "MMM do, yyyy")}</span>
+      <span className="text-md text-gray-500">Established: {format(org.createdAt, "MMM do, yyyy")}</span>
 
-      <div className="mt-4 grid grid-cols-2 gap-4 text-center">
+      {org.officers.length <= 1 && (
+        <div className="mt-2 grid grid-cols-1 gap-4 text-center">
         {(
           org.officers as unknown as { position: string; username: string }[]
         ).map(
-          (member) =>
+          async (member) =>
             member !== null && (
-              <Link key={member.position} href={`/user/${member.username}`}>
-                <div className="text-sm text-white">
-                  <div className="bg-teal-600 rounded-full px-2 py-1">
-                    <span className="block">{member.position}</span>
-                    <span className="block font-semibold">@{member.username}</span>
+              <Link key={member?.position} href={`/user/${member?.username}`}>
+                <div className="text-sm text-gray-700">
+                  <span className="block">{member?.position}</span>
+                  <div className="bg-teal-600 rounded-full px-2 py-1 mt-1 flex items-center ml-1">
+                    <Avatar className="w-6 h-6 mr-2">
+                      <AvatarImage src={await getUserPic(member?.username) ?? ""} alt={`${member?.username}`} />
+                      <AvatarFallback>{member?.username?.charAt(0).toUpperCase() ?? ""}</AvatarFallback>
+                    </Avatar>
+                    <span className="block font-semibold text-white">@{member?.username}</span>
                   </div>
                 </div>
               </Link>
             )
         )}
       </div>
+      )}
 
-      <span className="text-md text-gray-600 text-justify">
+    {org.officers.length > 1 && (
+        <div className="mt-2 grid grid-cols-2 gap-4 text-center">
+        {(
+          org.officers as unknown as { position: string; username: string }[]
+        ).map(
+          async (member) =>
+            member !== null && (
+              <Link key={member?.position} href={`/user/${member?.username}`}>
+                <div className="text-sm text-gray-700">
+                  <span className="block">{member?.position}</span>
+                  <div className="bg-teal-600 rounded-full px-2 py-1 mt-1 flex items-center ml-1">
+                    <Avatar className="w-6 h-6 mr-2">
+                      <AvatarImage src={await getUserPic(member?.username) ?? ""} alt={`${member?.username}`} />
+                      <AvatarFallback>{member?.username?.charAt(0).toUpperCase() ?? ""}</AvatarFallback>
+                    </Avatar>
+                    <span className="block font-semibold text-white">@{member?.username}</span>
+                  </div>
+                </div>
+              </Link>
+            )
+        )}
+      </div>
+    )}
+      
+
+      <span className="ml-6 mr-6 mt-6 text-md text-gray-800 text-justify">
         {org.description}
       </span>
     </div>
