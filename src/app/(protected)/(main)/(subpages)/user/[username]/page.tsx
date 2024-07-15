4dcbdd583cb092d118c5dd3637f8f1ef7fcef223
Hans Buhrs Aguilar <hbuhrsag@ttu.edu>
2024-07-15 11:04:03 -0600

    fixed minor not showing up correctly, and started the redesign

--- src/app/(protected)/(main)/(subpages)/user/[username]/page.tsx
+++ src/app/(protected)/(main)/(subpages)/user/[username]/page.tsx
@@ -20,26 +20,41 @@ export default async function UserPage({
   if (!user) return <div>User not found</div>;
 
   const degree = getDegreeByKey(user.major);
+  const minor = getDegreeByKey(String(user.minor));
 
   return (
     <div className="flex flex-col items-center w-auto h-auto text-center p-3 justify-center">
       <Avatar className="w-24 h-24">
         <AvatarImage src="" />
-        <AvatarFallback className="bg-[#D9D9D9]"></AvatarFallback>
+        <AvatarFallback className="bg-gray-300">
+          {user.firstName[0]}
+          {user.lastName[0]}
+        </AvatarFallback>
       </Avatar>
-      <h2>
-        {user.firstName} {user.lastName}
-      </h2>
-      <h4>
-        <span className="text-[#cc0000]">R-Number:</span> {user.r_number}
-      </h4>
-      <div
-        className="w-fit px-2 pt-0.5 pb-1 mt-2 rounded-full border"
-        style={{ color: degree?.color, borderColor: degree?.color }}
-      >
-        <span>{degree?.name}</span>
+      <div>
+        <h2>
+          {user.firstName} {user.lastName}
+        </h2>
+        <span className="text-gray-500">@{user.username}</span>
       </div>
-      <span>{user.minor == "NONE" ? false : user.minor}</span>
+      <div className="flex flex-row gap-2">
+        <div
+          className="text-white w-fit px-2 pt-0.5 pb-1 mt-2 rounded-full border"
+          style={{
+            backgroundColor: degree?.color /* , borderColor: degree?.color */,
+          }}
+        >
+          <span>{degree?.name}</span>
+        </div>
+        {user.minor !== "NONE" && (
+          <div
+            className="text-white w-fit px-2 pt-0.5 pb-1 mt-2 rounded-full border"
+            style={{ backgroundColor: minor?.color }}
+          >
+            <span>{minor?.name}</span>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
