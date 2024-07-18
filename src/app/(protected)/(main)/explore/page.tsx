6866e85779b0d5602bf4e8274b1034584ab620a4
Ezequiel Buck <ezequiel.bm04@gmail.com>
2024-07-17 21:09:18 -0600

    modified tailwind classes format for search bar

--- src/app/(protected)/(main)/explore/page.tsx
+++ src/app/(protected)/(main)/explore/page.tsx
@@ -5,10 +5,9 @@ export default function Explore() {
   return (
     <>
       <input
-        className="m-5 border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-3xl text-sm focus:outline-none"
+        className="m-5 h-10 w-auto px-5 pr-10 rounded-3xl border-2 border-gray-300 bg-white text-sm focus:outline-none"
         type="search"
         id="orgsearch"
-        name="orgsearch"
         placeholder="Search TTU@CR"
       />
       <OrgsList />
