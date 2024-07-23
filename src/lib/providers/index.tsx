075b0a3e3774ea7b60b169807a44d5582e1e249c
axantillon <aantillon.leiva@gmail.com>
2024-07-22 18:40:27 -0600

    fix build error

--- src/lib/providers/index.tsx
+++ src/lib/providers/index.tsx
@@ -1,20 +1,25 @@
 "use client";
 import { ThemeProvider } from "next-themes";
 import PullToRefresh from "pulltorefreshjs";
-import { ReactNode } from "react";
-import AuthProvider from "./authProvider";
+import { ReactNode, useEffect } from "react";
 import { detectOS } from "../utils";
+import AuthProvider from "./authProvider";
+import { useRouter } from "next/navigation";
 
 export const GlobalProviders = ({ children }: { children: ReactNode }) => {
 
-  if (detectOS() === "iOS") {
-    PullToRefresh.init({
-      mainElement: "body",
-      onRefresh() {
-        window.location.reload();
-      },
-    });
-  }
+  const router = useRouter();
+
+  useEffect(() => {
+    if (window && router && detectOS() === "iOS") {
+      PullToRefresh.init({
+        mainElement: "body",
+        onRefresh() {
+          router.refresh();
+        },
+      });
+    }
+  }, [router]);
 
   return (
     <ThemeProvider
