f5a8d24b4e34b90d57dd1b03f9356ff65938d58f
axantillon <aantillon.leiva@gmail.com>
2024-07-24 13:18:20 -0600

    finish simple settings page

--- src/app/(protected)/(main)/settings/page.tsx
+++ src/app/(protected)/(main)/settings/page.tsx
@@ -1,10 +1,44 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+import { ChevronRight, LogOut, Moon } from "react-feather";
+
 export default function Explore() {
   return (
     <div className="flex flex-col pt-8">
-      <span className="text-3xl font-bold">Settings</span>
+      <span className="text-3xl font-bold mb-6">Settings</span>
       <div className="flex flex-col gap-y-2">
+        <Link href="/settings/profile">
+          <SettingsItem>
+            <span>Profile</span>
+            <div className="flex items-center justify-center size-10 rounded-xl bg-black text-white">
+              <ChevronRight />
+            </div>
+          </SettingsItem>
+        </Link>
+        <SettingsItem>
+          <span>Dark Mode</span>
+          <div className="flex items-center justify-center size-10 rounded-xl bg-black text-white">
+            <Moon />
+          </div>
+        </SettingsItem>
+        <SettingsItem>
+          <span>Log Out</span>
+          <div className="flex items-center justify-center size-10 rounded-xl bg-red-500 text-white">
+            <LogOut />
+          </div>
+        </SettingsItem>
+      </div>
 
+      <div className="flex flex-col gap-y-1 text-xs text-black/50 text-center mt-6">
+        <span> made with ❤ by Students, for Students</span>
+        <span> - The Startup & Tech Lab</span>
       </div>
     </div>
   );
 }
+
+export const SettingsItem = ({children, onClick}: {children: ReactNode, onClick?: () => any}) => (
+  <div onClick={onClick} className="flex justify-between items-center w-full h-12 rounded-lg bg-white pl-4 pr-1 text-xl shadow-sm">
+    {children}
+  </div>
+)
--- src/components/utils/LogoutBtn.tsx
+++ src/components/utils/LogoutBtn.tsx
@@ -1,8 +1,9 @@
 "use client";
+import { SettingsItem } from "@/app/(protected)/(main)/settings/page";
 import { User } from "@prisma/client";
 import { usePrivy } from "@privy-io/react-auth";
+import { LogOut } from "lucide-react";
 import { useLocalStorage } from "usehooks-ts";
-import { Button } from "../ui/shadcn/button";
 
 export const LogoutBtn = () => {
   const { logout } = usePrivy();
@@ -10,14 +11,17 @@ export const LogoutBtn = () => {
   const [__, setUser] = useLocalStorage<User | null>("authUser", null);
 
   return (
-    <Button
+    <SettingsItem
       onClick={() => {
         setPrevAuth(false);
         setUser(null);
         logout();
       }}
     >
-      Log out
-    </Button>
+      <span>Log Out</span>
+      <div className="flex items-center justify-center size-10 rounded-xl bg-red-500 text-white">
+        <LogOut />
+      </div>
+    </SettingsItem>
   );
 };
