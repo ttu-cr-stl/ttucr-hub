75db7290bc14a24e4e66fc773f766bc352a1f17f
axantillon <aantillon.leiva@gmail.com>
2024-08-07 13:38:15 -0600

    remove annoying hover on avatar circles

--- src/components/magicui/avatar-circles.tsx
+++ src/components/magicui/avatar-circles.tsx
@@ -29,7 +29,7 @@ const AvatarCircles = ({
         />
       ))}
       {displayNum > 0 && (
-        <div className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white hover:bg-gray-600 dark:border-gray-800 dark:bg-white dark:text-black">
+        <div className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white dark:border-gray-800 dark:bg-white dark:text-black">
           +{displayNum}
         </div>
       )}
