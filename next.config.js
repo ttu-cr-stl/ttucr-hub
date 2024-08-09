c3712eeb9556d67c1f6156473b820a2f475e81fd
axantillon <aantillon.leiva@gmail.com>
2024-08-09 14:34:07 -0600

    Revert "disable pwa for now"
    This reverts commit ac9f93a43f5a32782449dc773c045d2e5d32f63a.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  disable: true, //process.env.NODE_ENV === "development",
+  disable: process.env.NODE_ENV === "development",
   register: true,
 });
 
