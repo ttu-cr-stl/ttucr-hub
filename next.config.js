54e0378090377f95ee1c24ffeb3dfc1a15b76b9b
Andres Antillon <aantillon.leiva@gmail.com>
2024-08-19 13:52:32 -0600

    test out speed efficiency

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: true, //process.env.NODE_ENV === "development",
   register: true,
 });
 
---
+++ src/app/(protected)/(main)/(subpages)/loading.tsx
@@ -0,0 +1,5 @@
+import { Skeleton } from "@/components/ui/shadcn/skeleton";
+
+export default function SubpageLoading() {
+  return <Skeleton className="rounded-lg w-full h-72 mt-12" />;
+}
