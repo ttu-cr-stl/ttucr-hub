eb40dd909bbeb08787b99588fa924d292aba6f56
Andres Antillon <aantillon.leiva@gmail.com>
2024-08-19 14:41:16 -0600

    modify icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,24 @@ export const metadata: Metadata = {
     title: APP_DEFAULT_TITLE,
     // startUpImage: [],
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png" },
+      { url: "/apple-touch-icon-57x57.png", sizes: "57x57" },
+      { url: "/apple-touch-icon-72x72.png", sizes: "72x72" },
+      { url: "/apple-touch-icon-76x76.png", sizes: "76x76" },
+      { url: "/apple-touch-icon-114x114.png", sizes: "114x114" },
+      { url: "/apple-touch-icon-120x120.png", sizes: "120x120" },
+      { url: "/apple-touch-icon-144x144.png", sizes: "144x144" },
+      { url: "/apple-touch-icon-152x152.png", sizes: "152x152" },
+      { url: "/apple-touch-icon-180x180.png", sizes: "180x180" },
+    ],
+  },
   formatDetection: {
     telephone: false,
   },
