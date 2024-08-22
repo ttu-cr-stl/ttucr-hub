73e1cce83de58e4daf3e8c369aaf8f700cd319ff
Andres Antillon <aantillon.leiva@gmail.com>
2024-08-22 10:15:32 -0600

    fix z index of avatar circles

--- src/components/magicui/avatar-circles.tsx
+++ src/components/magicui/avatar-circles.tsx
@@ -34,7 +34,7 @@ const AvatarCircles = ({
         </Avatar>
       ))}
       {displayNum > 0 && (
-        <div className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white dark:border-gray-800 dark:bg-white dark:text-black">
+        <div className="z-20 flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-black text-center text-xs font-medium text-white dark:border-gray-800 dark:bg-white dark:text-black">
           +{displayNum}
         </div>
       )}
